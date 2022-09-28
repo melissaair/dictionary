f15f6920fbf124f195d518e2e542cba3f09e1dea
melissaair <100801990+melissaair@users.noreply.github.com>
2022-09-28 14:55:52 -0700

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -6,14 +6,13 @@ export default function Dictionary() {
 	let [keyword, setKeyword] = useState(null);
 
 	function handleResponse(response) {
-		console.log(response);
+		console.log(response.data[0]);
 	}
 
 	function search(event) {
 		event.preventDefault();
-		alert(`Searching for ${keyword} definition`);
 
-		let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/sunset";
+		let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 		axios.get(apiUrl).then(handleResponse);
 	}
 
